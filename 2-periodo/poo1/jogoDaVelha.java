6366764b0748eb03c80040c13ab6a0ef85a99b05
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-09-26 13:42:02 -0300

    Update jogoDaVelha.java

--- 2-periodo/poo1/jogoDaVelha.java
+++ 2-periodo/poo1/jogoDaVelha.java
@@ -8,7 +8,9 @@ public class jogoDaVelha {
 
     static int tabuleiro [][] = new int [3][3];
 
-    public static void main(String[] args){
+        public static void main(String[] args){
+
+        // Preenche matriz "tabuleiro" com 0 (vazio)
         for (int i=0; i<3; i++) {
             for (int j=0; j<3; j++) {
                 tabuleiro[i][j] = 0;
@@ -16,23 +18,17 @@ public class jogoDaVelha {
         }
         
         int jogador = 1;
-
-        int contJogadas = 0;
+        int posicao = 0;
+        int contJogadas = 1;
         
-        while () {
-            int posicao = interfaceTabuleiro(jogador);
+        while ((vitoria() != true) && (empate() != true)) {
+            posicao = interfaceTabuleiro(jogador);
             
             // Se a posição estiver vazia [0], ele preenche de acordo com o respectivo jogador
             if (validacao(posicao)) {
                 contJogadas++;
-                
-                // Caso a qnt de jogadas válidas for PAR é o jogador 1, caso for IMPAR é o jogador 2
-                if (contJogadas%2 == 0) {
-                    jogador = 1;
-                } else {
-                    jogador = 2;
-                }
-                
+        
+                // Após recebida a jogada, verifica cada uma das 9 posições para preencher no tabuleiro
                 if (posicao == 1) {
                     if (jogador == 1) {
                     tabuleiro[0][0] = 1; 
@@ -105,8 +101,18 @@ public class jogoDaVelha {
                         tabuleiro[2][2] = 2; 
                     }
                 }
+
+                // Alterna jogador
+                // Se o jogador for = 1, recebe o '2', e se for = 2, recebe o '1'
+                jogador = (jogador == 1) ? 2 : 1;
+
+
+            } else {
+                restricao();
             }
+
         }
+        exibeFim(jogador == 1 ? 2 : 1);  // O jogador que fez a última jogada é o vencedor
     }
 
 
@@ -118,27 +124,32 @@ public class jogoDaVelha {
         
         for (int i=0; i<3; i++) {
             for (int j=0; j<3; j++) {
-                System.out.printf(" %d ", tabuleiro[i][j]);
                 if (tabuleiro[i][j] == 1) {
-                    System.out.print("X");
+                    System.out.print(" X");
                 } 
                 else if (tabuleiro[i][j] == 2) {
-                    System.out.print("O");
+                    System.out.print(" O");
                 } 
                 else {
-                    System.out.print(" ");
+                    System.out.print(" -");
                 }
             }
             System.out.println();
         }
 
-        System.out.println("\n1 2 3");
-        System.out.println("4 5 6");
-        System.out.println("7 8 9\n");
+        System.out.println("\n 1 2 3");
+        System.out.println(" 4 5 6");
+        System.out.println(" 7 8 9\n");
 
-        System.out.printf("Jogador '%d' é a sua vez de jogar!\n", jogador);
+
+        if (jogador == 1) {
+            System.out.println("Jogador 'X' é a sua vez de jogar!");
+        } else {
+            System.out.println("Jogador 'O' é a sua vez de jogar!");
+        }
+
         
-        System.out.println("Escolha uma posição: ");
+        System.out.print("Escolha uma posição: ");
         int jogada = s.nextInt();
 
         return jogada;
@@ -211,27 +222,88 @@ public class jogoDaVelha {
                 return false;
             }
         }
+        return false;
     }
 
-    // // 3)
-    // static void restricao() {
+    // 3)
+    static void restricao() {
+        System.out.println("\n- - JOGADA INVÁLIDA - -\n");
+    }
 
-    // }
+    // 4)
+    static boolean empate(){
+        // Verificamos se o tabuleiro está cheio
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tabuleiro[i][j] == 0) {  // 0 representa uma célula vazia
+                    return false;  // Jogo ainda não acabou, há células vazias
+                }
+            }
+        }
 
-    // // 4)
-    // static boolean empate(){
+        // Jogo deu empate (velha)
+        return true;   
+    }
 
-    // }
+    // 5)
+    static boolean vitoria() {
+        for (int i = 0; i < 3; i++) {
+            // Se o primeiro elemento é dif de 0 e o restante da LINHA forem iguais
+            if ((tabuleiro[i][0] != 0) && (tabuleiro[i][0] == tabuleiro[i][1]) && (tabuleiro[i][1] == tabuleiro[i][2])) {
+                return true;
+            }
+    
+            // Se o primeiro elemento é dif de 0 e o restante da COLUNA forem iguais
+            if ((tabuleiro[0][i] != 0) && (tabuleiro[0][i] == tabuleiro[1][i]) && (tabuleiro[1][i] == tabuleiro[2][i])) {
+                return true;
+            }
+        }
+    
+        // Se o primeiro elemento é dif de 0 e o restante PRIMEIRA DIAGONAL forem iguais
+        if ((tabuleiro[0][0] != 0) && (tabuleiro[0][0] == tabuleiro[1][1]) && (tabuleiro[1][1] == tabuleiro[2][2])) {
+            return true;
+        }
+    
+        // Se o primeiro elemento é dif de 0 e o restante SEGUNDA DIAGONAL forem iguais
+        if ((tabuleiro[2][0] != 0) && (tabuleiro[2][0] == tabuleiro[1][1]) && (tabuleiro[1][1] == tabuleiro[0][2])) {
+            return true;
+        }
+    
+        return false;
+    }
 
-    // // 5)
-    // static boolean vitoria(){
+    // 6)
+    static void exibeFim(int jogador){
 
-    // }
+        System.out.println("\n  __ FIM __");
+        for (int i=0; i<3; i++) {
+            System.out.print("  |");
+            for (int j=0; j<3; j++) {
+                if (tabuleiro[i][j] == 1) {
+                    System.out.print(" X");
+                } 
+                else if (tabuleiro[i][j] == 2) {
+                    System.out.print(" O");
+                } 
+                else {
+                    System.out.print(" -");
+                }
+            }
+            System.out.print(" |");
+            System.out.println();
+        }
 
-    // // 6)
-    // static void exibeFim(int){
+        if (empate()) {
+            System.out.println("--> DEU VELHA :/\n");
+        }
+        else if (jogador == 1){
+            System.out.println( "\n--> PARABENS!! JOGADOR 'X' GANHOU\n");
 
-    // }
+        } 
+        else if (jogador == 2) {
+            System.out.println("\n--> PARABENS!! JOGADOR 'O' GANHOU\n");
+        }
+    }
 
     
 }
