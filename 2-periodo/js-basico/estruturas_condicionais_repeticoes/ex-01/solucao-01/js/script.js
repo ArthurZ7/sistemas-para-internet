807d04ce35d3fd3376443a1700c2aa320859a4d7
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-10-03 22:10:38 -0300

    Delete 2-periodo/js-basico/estruturas_condicionais_repeticoes directory

--- 2-periodo/js-basico/estruturas_condicionais_repeticoes/ex-01/solucao-01/js/script.js
+++
@@ -1,5 +0,0 @@
-const dataAtual = new Date();
-
-var diasSemana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
-
-document.write(`Hoje é ${diasSemana[dataAtual.getDay()]}!`)
