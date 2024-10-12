bf21035e52ce513e127736a1da62e7b98ac8d9df
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-10-12 12:06:12 -0300

    Delete 2-periodo/js-basico/array_e_numeros_aleatorios directory

--- 2-periodo/js-basico/array_e_numeros_aleatorios/1-aleat_1_ate_10/js/script.js
+++
@@ -1,6 +0,0 @@
-// 1. Construa uma página web para exibir no corpo da página (document.write) um número
-// aleatório entre 1 e 10.
-
-var nroAleatorio = parseInt(Math.random()*10)+1;
-
-document.write("<h1>" + nroAleatorio + "</h1>")
