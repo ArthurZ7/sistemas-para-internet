4da89dfeab4966fee85afa7c5d58566e25ba6ded
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-10-21 22:17:16 -0300

    21/10/2024

---
+++ 2-periodo/pmvc/hello_world/src/main/java/br/edu/iftm/tspi/pmvc/PmvcApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.pmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmvcApplication.class, args);
+	}
+
+}
---
+++ 2-periodo/pmvc/hello_world/src/test/java/br/edu/iftm/tspi/pmvc/PmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.tspi.pmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
