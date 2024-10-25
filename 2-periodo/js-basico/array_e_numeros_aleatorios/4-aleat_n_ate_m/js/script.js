865b3fc5ef0a3adf917ebcde2243590b4e4a7957
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-10-24 22:21:28 -0300

    Delete 2-periodo/js-basico/array_e_numeros_aleatorios directory

--- 2-periodo/js-basico/array_e_numeros_aleatorios/4-aleat_n_ate_m/js/script.js
+++
@@ -1,9 +0,0 @@
-// 4. Construa uma página web para exibir no corpo da página (document.write) um número
-// aleatório entre N e M, onde N e M deverão ser informados pelo usuário.
-
-num1 = parseInt(prompt("Insira um número inicial: "))
-num2 = parseInt(prompt("Insira um número final: "))
-
-var nroAleatorio = parseInt(Math.random()*num2)+num1;
-
-document.write("<h1>" + nroAleatorio + "</h1>")
