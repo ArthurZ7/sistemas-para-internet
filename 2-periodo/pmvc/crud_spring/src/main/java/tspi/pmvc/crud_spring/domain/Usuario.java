05d5acd9d256ec95887b920cb88c801a04ffeb0a
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-11-14 11:21:21 -0300

    11/11/24

---
+++ 2-periodo/pmvc/crud_spring/src/main/java/tspi/pmvc/crud_spring/domain/Usuario.java
@@ -0,0 +1,56 @@
+package tspi.pmvc.crud_spring.domain;
+
+public class Usuario {
+
+    private String nome;
+
+    private String login;
+
+    private String senha;
+
+    private Boolean administrador;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String login, String senha, Boolean administrador) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.administrador = administrador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(Boolean administrador) {
+        this.administrador = administrador;
+    }
+
+
+}
