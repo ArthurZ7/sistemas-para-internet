1f24b084dd202de89ceb049040ee1c17be97fb83
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-11-14 11:19:55 -0300

    Atv Avaliativa - Static

---
+++ atv_comp_retas/etapa_3/Validacao.java
@@ -0,0 +1,12 @@
+package etapa_3;
+
+public class Validacao {
+    
+    public static boolean valida (double x1, double y1, double x2, double y2) {
+        if (x1 == x2 && y1 == y2) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
